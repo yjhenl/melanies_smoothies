d51b5370475ed62dd7d697567058316f0f459dbd
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 11:36:07 +0200

    Display smoothie response in the app
    Replaced commented out write statement with text display of the smoothie response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
 
     import requests  
     smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-    #st.write(my_insert_stmt)
+    st.text(smoothiefroot_response)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
@@ -55,7 +55,6 @@ if ingredients_list:
 
 
 
-st.text(smoothiefroot_response)
 
 
 
