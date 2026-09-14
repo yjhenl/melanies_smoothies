bf4c4d274f84bfb16465cb237f9dca1d677e3489
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 11:46:53 +0200

    Fix URL formatting in API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
     st.write(my_insert_stmt)
     #st.stop()
     import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/" + watermelon + "](https://my.smoothiefroot.com/api/fruit/" + watermelon + ")")
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + watermelon)
     st.text(smoothiefroot_response)
     time_to_insert = st.button('Submit Order')
 
