9d021d29b812cd0ba79da8a91c597d6d5994392d
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 17:09:38 +0200

    Refactor data display to use Pandas DataFrame
    Updated the Streamlit app to display a Pandas DataFrame instead of a Snowpark DataFrame. Added comments and modified the data retrieval process for fruit options.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,15 @@ st.write("The name on your Smoothie will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+# Show the table in App
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+
+# Covert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
@@ -32,6 +39,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrient Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
