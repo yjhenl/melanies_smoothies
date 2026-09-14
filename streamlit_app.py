8e319b86f12f83525271f7ffbb0d94941e94cb51
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 12:15:57 +0200

    Comment out SQL insert statement logging
    Comment out the SQL insert statement write operation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
 
-    st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
     #st.stop()
     import requests  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
