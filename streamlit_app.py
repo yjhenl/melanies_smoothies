b4054ae2185b8f2ac2cce5942db485fb09003d8a
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 17:20:08 +0200

    Fix string formatting for API request URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrient Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
       
     #st.write(ingredients_string)
