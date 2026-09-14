f568434286cc08a5f6ac4f74168f53d4978b48b6
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 12:10:17 +0200

    Display watermelon data in a dataframe
    Add dataframe display for watermelon data from API response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,8 @@ if ingredients_list:
     import requests  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
     st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+  
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
