2a7ff7652a85c2e98f079ee3b695dfa42f09ab2c
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 12:05:04 +0200

    Change response output to JSON format

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
     #st.stop()
     import requests  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
