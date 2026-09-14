d870a5b7c338b083903506fae9d9e53871cb3c70
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 11:38:22 +0200

    Reorganize requests import and API call
    Moved the requests import and API call to a different location in the code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,9 @@ if ingredients_list:
 
     st.write(my_insert_stmt)
     #st.stop()
-
+    import requests  
+    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+    st.text(smoothiefroot_response)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
@@ -50,9 +52,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-    import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-    st.text(smoothiefroot_response)
 
 
 
