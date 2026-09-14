bd819f34a7a5777fd55bdbf984b1849b9a7e6ebb
Yi Jhen Li <104784870+yjhenl@users.noreply.github.com>
2026-09-14 12:17:15 +0200

    Comment out JSON response display
    Comment out the line displaying the JSON response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
     #st.stop()
     import requests  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    st.text(smoothiefroot_response.json())
+    #st.text(smoothiefroot_response.json())
     sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
   
     time_to_insert = st.button('Submit Order')
